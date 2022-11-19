1662389bb7333fd63ef04911f750b6e81d6346f3
firhoe <fiorela.ngr@gmail.com>
2022-11-18 20:39:26 -0500

    feat: agregar formulario popup responsivo

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+// initiates profile editor popup
+
+const openFormButton = document.querySelector('.profile__edit-button');
+
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.popup__close-button');
+
+function toggleForm() {
+  popup.classList.toggle('popup_opened');
+}
+
+openFormButton.addEventListener('click', toggleForm);
+closeButton.addEventListener('click', toggleForm);
+
+// initiates profile editor popup save button
+
+const form = document.querySelector('.popup__form');
+
+const profileName = document.querySelector('.profile__user');
+const profileAbout = document.querySelector('.profile__profession');
+
+const inputName = document.getElementById('popup_input_name');
+const inputAbout = document.getElementById('popup_input_about');
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileAbout.textContent = inputAbout.value;
+  console.log(profileName.textContent);
+}
+
+form.addEventListener('submit', handleFormSubmit);
